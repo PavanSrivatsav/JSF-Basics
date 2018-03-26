3dbcf3b06ba6a95cbd87d07b74f1e93763be4ee1
PavanSrivatsav <pavansrivatsav96@gmail.com>
2018-03-26 07:21:04 +0530

    sample-dropdown from bean

--- src/com/pavansrivatsav/jsf/beans/Student.java
+++ src/com/pavansrivatsav/jsf/beans/Student.java
@@ -1,6 +1,8 @@
 package com.pavansrivatsav.jsf.beans;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.faces.bean.ManagedBean;
 
@@ -12,11 +14,22 @@ public class Student implements Serializable {
 	private String firstName;
 	private String lastName;
 	private String country;
+	private List<String> countries;
 
 	public Student() {
+
+		countries = new ArrayList<String>();
+		countries.add("USA");
+		countries.add("IND");
+		countries.add("UAE");
+
 	}
 
 	/** Getters and Setters **/
+
+	public List<String> getCountries() {
+		return countries;
+	}
 
 	public String getFirstName() {
 		return firstName;
